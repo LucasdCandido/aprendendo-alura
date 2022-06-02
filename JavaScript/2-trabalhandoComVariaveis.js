e3592e6a1def9aafe61040c6e66bad559261e0c5
LucasdCandido <lucasdcandido1994@gmail.com>
2022-06-02 14:03:26 -0300

    estudo de javascript

--- JavaScript/2-trabalhandoComVariaveis.js
+++ JavaScript/2-trabalhandoComVariaveis.js
@@ -1,1 +1,25 @@
-console.log("Trabalhando com variaveis");
+const idadeComprador = 21;
+const estaAcompanhado = true;
+let passagemComprada = false;
+const destino = 'Rio de Janeiro';
+const listaDeDestino = [
+  'São Paulo',
+  'Rio de Janeiro',
+  'Recife'
+];
+
+console.log('Destinos possiveis: ');
+console.log(listaDeDestino);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhado == true;
+
+let contador = 0;
+while(contador<3) {
+  if (listaDeDestino[contador] == destino){
+    console.log(`Temos ${destino}!`);
+    break
+  }else{
+    console.log('Destino não existe');
+  }
+  contador ++;
+};
