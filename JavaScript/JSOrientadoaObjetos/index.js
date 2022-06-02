c1f239811e35b881b0e8a2bdc077f2ab4e8b9b30
LucasdCandido <lucasdcandido1994@gmail.com>
2022-06-02 18:13:10 -0300

    começando estudo orientado a objeto

---
+++ JavaScript/JSOrientadoaObjetos/index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = 'Ricardo';
+const Cliente1Cpf = 11122233309;
+const Cliente1Agencia = 1001;
+const Cliente1Saldo = 0;
+
+const cliente2Nome = 'Alice';
+const Cliente2Cpf = 88822233309;
+const Cliente2Agencia = 1001;
+const Cliente2Saldo = 0;
+
+console.log(cliente1Nome);
