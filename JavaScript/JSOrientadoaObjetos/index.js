49b4d871f1a73171815b5898955cc719dad3c60c
Lucas Dourado Candido <lucasdcandido1994@gmail.com>
2022-06-02 21:56:20 -0300

    continuando estudo de jsoo

--- JavaScript/JSOrientadoaObjetos/index.js
+++ JavaScript/JSOrientadoaObjetos/index.js
@@ -1,11 +1,25 @@
-const cliente1Nome = 'Ricardo';
-const Cliente1Cpf = 11122233309;
-const Cliente1Agencia = 1001;
-const Cliente1Saldo = 0;
+class Cliente{
+  nome;
+  cpf;
+  agencia;
+  saldo;
+}
+class ContaCorrente{
+  
+}
 
-const cliente2Nome = 'Alice';
-const Cliente2Cpf = 88822233309;
-const Cliente2Agencia = 1001;
-const Cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
-console.log(cliente1Nome);
+cliente1.nome = 'Ricardo';
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = 'Alice';
+cliente2.cpf = 88822233309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
