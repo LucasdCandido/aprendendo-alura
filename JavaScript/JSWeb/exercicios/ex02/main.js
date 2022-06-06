f32ef28fa3a94e482e3dadc8df0fdbd4b788e3af
LucasdCandido <lucasdcandido1994@gmail.com>
2022-06-06 17:00:34 -0300

    avançando no estudo de javascript web

--- JavaScript/JSWeb/exercicios/ex02/main.js
+++ JavaScript/JSWeb/exercicios/ex02/main.js
@@ -4,4 +4,4 @@ const numero = document.querySelectorAll('input[type=button]');
 function imprimirNumero(){
   alert(numero);
 }
-document.querySelector('input[type=tel]').onclick = imprimirNumero;
+document.querySelectorAll('input[type=button]').onclick = imprimirNumero;
--- JavaScript/JSWeb/js/main.js
+++ JavaScript/JSWeb/js/main.js
@@ -1,11 +1,28 @@
-function tocaSom(num){
-  let sons = document.querySelectorAll('audio');
-  sons[num].play();
+function tocaSom(idElementoAudio){
+
+  document.querySelector(idElementoAudio).play();
 }
-
-
-
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom; 
+
+//para
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;//template string
+
+
+  tecla.onclick = function(){
+    tocaSom(idAudio)
+  };
+
+  tecla.onkeydown = function() {
+    listaDeTeclas.classList.add('.ativa');
+  }
+  tecla.onkeyup = function() {
+    listaDeTecla.classList.remove('.ativa');
+  }
+
+}
